31f0edbf0d9400562a8f50692998382a75101a33
Andrea Gaetano Scardaci <andreagaetanoscardaci04@gmail.com>
2026-09-01 16:02:38 +0200

    Diagnostica: testa il solo import diretto di nodemailer, senza createTransport

--- api/ping.ts
+++ api/ping.ts
@@ -1,5 +1,5 @@
-import { transporter } from './_lib/mail';
+import nodemailer from 'nodemailer';
 
 export default function handler(req: any, res: any) {
-  res.status(200).json({ ok: true, step: 'with-mail-import', hasTransporter: !!transporter });
+  res.status(200).json({ ok: true, step: 'with-nodemailer-import-only', hasFn: typeof nodemailer.createTransport });
 }
